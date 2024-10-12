0abf16f042ac7a99c28c21871a68e7e73b602411
virtualabishek <717abishekneupane@gmail.com>
2024-10-12 14:40:10 +0545

    solving deployment issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/virtualabishek.github.io", // Replace with your GitHub Pages repository name
+  assetPrefix: "/virtualabishek.github.io", // Same as basePath
   webpack(config) {
-    // Grab the existing rule that handles SVG imports
+    // SVG rule setup as you already have
     const fileLoaderRule = config.module.rules.find((rule) =>
       rule.test?.test?.(".svg")
     );
 
     config.module.rules.push(
-      // Reapply the existing rule, but only for svg imports ending in ?url
       {
         ...fileLoaderRule,
         test: /\.svg$/i,
         resourceQuery: /url/, // *.svg?url
       },
-      // Convert all other *.svg imports to React components
       {
         test: /\.svg$/i,
         issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
         use: {
           loader: "@svgr/webpack",
           options: {
@@ -38,7 +42,6 @@ const nextConfig = {
       }
     );
 
-    // Modify the file loader rule to ignore *.svg, since we have it handled now.
     fileLoaderRule.exclude = /\.svg$/i;
 
     return config;
