3299cf0947aa724cfc53ab7d2c2980f63f826c68
virtualabishek <717abishekneupane@gmail.com>
2024-12-24 16:35:05 +0545

    Fix hosting issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { BrowserRouter } from "react-router-dom";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import App from "./App";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
