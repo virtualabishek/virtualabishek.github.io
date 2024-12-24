910ff727dda456ea530504dbdd62a9630d62f88d
virtualabishek <717abishekneupane@gmail.com>
2024-12-24 20:21:41 +0545

    Replace HashRouter with BrowserRouter and update Vite base path for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-createRoot(document.getElementById("root")).render(
-  <HashRouter>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
